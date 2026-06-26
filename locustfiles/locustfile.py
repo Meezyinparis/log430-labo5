a7fe4f44076e33ca57b8b13bbc42ca491ec2ad5f
Emmanuel Senosier <98911028+Meezyinparis@users.noreply.github.com>
2026-06-26 15:16:27 -0400

    locus read and write

--- locustfiles/locustfile.py
+++ locustfiles/locustfile.py
@@ -4,62 +4,33 @@ SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 import random
-from locust import task, between
-from locust.contrib.fasthttp import FastHttpUser
-import logging
+from locust import HttpUser, task, between
 
 
-class FlaskAPIUser(FastHttpUser):
-    # Temps d'attente entre les requêtes (1 à 2 secondes)
-    wait_time = between(1, 2)
+class FlaskAPIUser(HttpUser):
+    # Wait time between requests (1-3 seconds)
+    wait_time = between(1, 3)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.logger = logging.getLogger(self.__class__.__name__)
-
-    # Proportion d'exécution 1:1:1, ce qui signifie : 1/3, 1/3, 1/3 (30 % des appels à chacun)
+    # Proportion 1:2:2, meaning: 1/5, 2/5, 2/5, because 1 + 2 + 2 = 5
+    # This means 20% calls to /orders, 40% calls to /highest-spenders, 40% calls to /best-sellers
     @task(1)
     def orders(self):
         """Test POST /orders endpoint (write)"""
-        random_id = random.randrange(1, 3)
-        random_id2 = random.randrange(1, 3)
-        random_id3 = random.randrange(1, 5)
         mock_order = {
-            "user_id": random_id,
-            "items": [{"product_id": random_id2, "quantity": random_id3}]
+            "user_id": random.randint(1, 3),
+            "items": [{"product_id": random.randint(1, 4), "quantity": random.randint(1, 10)}]
         }
 
-        # Ajouter aléatoirement plusieurs articles (30 % des fois)
+        # Randomly add a second item (30% of times)
         if random.randint(1, 10) <= 3:
-            random_id = random.randrange(1, 3)
             mock_order["items"].append(
-                {"product_id": random_id, "quantity": 1})
-            mock_order["items"].append(
-                {"product_id": random_id, "quantity": 2})
+                {"product_id": random.randint(1, 4), "quantity": random.randint(1, 10)})
 
-        with self.client.post("/store-manager-api/orders",
+        with self.client.post("/orders",
                               json=mock_order,
                               headers={"Content-Type": "application/json"},
                               catch_response=True) as response:
             try:
-
-                if response is None or response.headers is None:
-                    self.environment.events.request.fire(
-                        request_type="POST",
-                        name="/orders",
-                        response_time=0,
-                        response_length=0,
-                        exception=Exception(
-                            "Aucune réponse (erreur ou timeout)")
-                    )
-                    return
-
-                content_type = response.headers.get('content-type', '').lower()
-                if 'application/json' not in content_type:
-                    response.failure(
-                        f"Erreur : {response.status_code} - Aucun JSON dans la réponse. Message : {response.text}")
-                    return
-
                 data = response.json()
                 if response.status_code == 201:
                     if "order_id" in data:
@@ -71,81 +42,40 @@ class FlaskAPIUser(FastHttpUser):
                     response.failure(
                         f"Erreur : {response.status_code} - {data.get('error', 'Unknown error')}")
             except ValueError:
-                response.failure(
-                    f"JSON invalide dans la réponse: {response.text}")
+                response.failure(f"Invalid JSON response: {response.text}")
 
-    @task(1)
+    @task(2)
     def highest_spenders(self):
         """Test GET /orders/reports/highest-spenders endpoint (read)"""
-        self.logger.info("Calling highest_spenders")
-        with self.client.get("/store-manager-api/orders/reports/highest-spenders", catch_response=True) as response:
+        with self.client.get("/orders/reports/highest-spenders", catch_response=True) as response:
             try:
-
-                if response is None or response.headers is None:
-                    self.environment.events.request.fire(
-                        request_type="GET",
-                        name="/orders/reports/highest-spenders",
-                        response_time=0,
-                        response_length=0,
-                        exception=Exception(
-                            "Aucune réponse (erreur ou timeout)")
-                    )
-                    return
-
-                content_type = response.headers.get('content-type', '').lower()
-                if 'application/json' not in content_type:
-                    response.failure(
-                        f"Erreur : {response.status_code} - Aucun JSON dans la réponse. Message : {response.text}")
-                    return
-
                 data = response.json()
                 if response.status_code == 200:
-                    if isinstance(data, list):
+                    if str(type(data)) == "<class 'list'>":
                         response.success()
                     else:
                         response.failure(
-                            "Le résultat n'est pas une liste : " + str(data))
+                            "Le resultat n'est pas une liste : " + str(data))
                 else:
                     response.failure(
                         f"Erreur : {response.status_code} - {data.get('error', 'Unknown error')}")
             except ValueError:
-                response.failure(
-                    f"Erreur : {response.status_code} - JSON invalide dans la réponse")
+                response.failure(f"Invalid JSON response: {response.text}")
 
-    @task(1)
+    @task(2)
     def best_sellers(self):
         """Test GET /orders/reports/best-sellers endpoint (read)"""
-        self.logger.info("Calling best_sellers")
         with self.client.get("/orders/reports/best-sellers", catch_response=True) as response:
             try:
-
-                if response is None or response.headers is None:
-                    self.environment.events.request.fire(
-                        request_type="GET",
-                        name="/store-manager-api/orders/reports/best-sellers",
-                        response_time=0,
-                        response_length=0,
-                        exception=Exception(
-                            "Aucune réponse (erreur ou timeout)")
-                    )
-                    return
-
-                content_type = response.headers.get('content-type', '').lower()
-                if 'application/json' not in content_type:
-                    response.failure(
-                        f"Erreur : {response.status_code} - Aucun JSON dans la réponse. Message : {response.text}")
-                    return
-
                 data = response.json()
                 if response.status_code == 200:
-                    if isinstance(data, list):
+                    if str(type(data)) == "<class 'list'>":
                         response.success()
                     else:
                         response.failure(
-                            "Le résultat n'est pas une liste : " + str(data))
+                            "Le resultat n'est pas une liste : " + str(data))
                 else:
                     response.failure(
                         f"Erreur : {response.status_code} - {data.get('error', 'Unknown error')}")
             except ValueError:
-                response.failure(
-                    f"Erreur : {response.status_code} - JSON invalide dans la réponse")
+                response.failure(f"Invalid JSON response: {response.text}")
